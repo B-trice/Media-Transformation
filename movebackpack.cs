7e5eb8c91f01d76e8457240fced650748de461b3
alexandrasophiapetersen <34125252+alexandrasophiapetersen@users.noreply.github.com>
2019-02-28 23:44:04 +0100

    Add files via upload

---
+++ movebackpack.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class movebackpack : MonoBehaviour {
+	Vector3 checkpoint;
+	[SerializeField] private Light Fahrrad;
+	public GameObject Funktion;
+	[SerializeField] private Canvas customCanvas;
+	[SerializeField] private Light Text1;
+	[SerializeField] private Light Text2;
+	[SerializeField] private Canvas Text;
+	
+void Start () {
+	
+	checkpoint = new Vector3 (78,-54,-45);
+	
+}
+
+void OnTriggerStay(Collider other) {
+			
+		if (Input.GetButton("Fire2"))
+        {
+            transform.position = checkpoint;
+			Fahrrad.enabled = true;
+			Funktion.SetActive(true);
+			customCanvas.enabled = false;
+			Text2.enabled = true;
+			Text1.enabled = false;
+			Text.enabled = true;
+			
+        }	
+    }
+	
+}
